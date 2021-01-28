1fde6e3bdad756c11431b3b1b5c034086b51eaa7
Andrea Pasquetto <a.pasquetto01@gmail.com>
2021-01-28 22:56:10 +0100

    customization

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 A simple screen locker utility for X written in C.
 
+> All settings can be modified in [config.h](./config.h)
+
 ## Patches
 
 - [capscolor](https://tools.suckless.org/slock/patches/capscolor)
 - [dpms](https://tools.suckless.org/slock/patches/dpms)
 - [message](https://tools.suckless.org/slock/patches/message)
+
+## Options
+
+| Option        | Action             |
+| ------------- | ------------------ |
+| `-m [string]` | set custom message |
--- config.h
+++ config.h
@@ -1,25 +1,18 @@
-/* user and group to drop privileges to */
-static const char *user = "nobody";
-static const char *group = "nogroup";
 
-static const char *colorname[NUMCOLS] = {
-  [INIT] = "black",     /* after initialization */
-  [INPUT] = "#005577",  /* during input */
-  [FAILED] = "#CC3333", /* wrong password */
-  [CAPS] = "red",       /* CapsLock on */
+/* appearance */
+static char *user       = "nobody";  /* user for privileges */
+static char *group      = "nobody";  /* group for privileges */
+static int failonclear  = 1;         /* cleared input = wrong password */
+static int monitortime  = 300;       /* time before disabling the monitor */
+static char *message    = "";        /* default message */
+static char *text_color = "#212121"; /* text color */
+static char *font_name  = "6x13";    /* text font */
+
+/* colors */
+static char *colorname[NUMCOLS] = {
+  [INIT]   = "#000000", /* initial status       */
+  [INPUT]  = "#1976d2", /* input                */
+  [FAILED] = "#b71c1c", /* error                */
+  [CAPS]   = "#fdd835", /* XK_Caps_Lock enabled */
 };
 
-/* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
-
-/* time in seconds before the monitor shuts down */
-static const int monitortime = 5;
-
-/* default message */
-static const char *message = "Suckless: Software that sucks less.";
-
-/* text color */
-static const char *text_color = "#ffffff";
-
-/* text size (must be a valid size) */
-static const char *font_name = "6x10";
